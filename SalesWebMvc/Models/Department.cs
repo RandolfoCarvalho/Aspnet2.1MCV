949e7e8aba416f324bd7de0c77edba10d7307476
RandolfoCarvalho <randolfoirades@gmail.com>
2023-12-23 15:52:42 -0300

    Other Entities and second Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,35 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        //usamos o ICollection aqui por que é uma coleção genérica e pode aceitar List, Hash etc...
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Department() 
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //soma o total de sales de cada vendedor que esta contido em Sellers, do departamento em questão
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
     }
+    
 }
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+        public DateTime birthDate { get; set; }
+        public double baseSalary { get; set; }
+        public Department Department { get; set; }
+        //ou seja, o que esse ICollection esta fazendo é uma associação 1 para N 
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            this.email = email;
+            this.birthDate = birthDate;
+            this.baseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+
+
+}
